c1782838896cff6ef23ee9bebb23f34394880150
Sergey Zhusman <zhusmansan@gmail.com>
2019-04-26 16:50:04 +0300

    Add Load More button

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,5 @@
+var currentPage = 1;
+
 $(document).ready(function () {
     var t = $(".js-search-field");
     var e = $(".js-top-bar");
@@ -6,28 +8,32 @@ $(document).ready(function () {
             e.addClass("_search-open")
         }, "blur": function () {
             e.removeClass("_search-open")
-        }, "keydown": function (e) { if(13 == e.keyCode){t.blur();clearQuestionsDiv();requestData(); } }
+        }, "keydown": function (e) { if (13 == e.keyCode) { t.blur(); clearQuestionsDiv(); requestData(); } }
     });
-    $(".js-search-submit").on({"click": function (e) {{t.blur();clearQuestionsDiv();requestData(); }} });
+    $(".js-search-submit").on({ "click": function (e) { { t.blur(); clearQuestionsDiv(); requestData(); } } });
     $("#warning").click(function () {
 
-        $(this).slideUp("slow"); 
-        return false; 
-        }); 
-        
+        $(this).slideUp("slow");
+        return false;
+    });
+    $("#load-more").click(function () {
+        requestData();
+    });
 });
 
-function requestData(){
+function requestData() {
 
 
     $.ajax({
         type: 'GET',
         url: "/search",
         data: {
-           intitle:$(".js-search-field")[0].value,
-           site:"stackoverflow",
-           order:"desc",
-           sort:"activity"
+            intitle: $(".js-search-field")[0].value,
+            site: "stackoverflow",
+            order: "desc",
+            sort: "activity",
+            pagesize: 3,
+            page: currentPage++
         },
         dataType: 'json',
         cache: false,
@@ -40,30 +46,32 @@ function requestData(){
     });
 }
 
-function clearQuestionsDiv(){
+function clearQuestionsDiv() {
     var questionsDiv = $('#questions');
-    questionsDiv[0].innerHTML="";
+    questionsDiv[0].innerHTML = "";
+    currentPage =1 ;
 
 }
 
-function processResponce(resp){
-    
+function processResponce(resp) {
+    $('#load-more')[0].disabled = !resp.has_more;
+
     $('#questionTemplate').tmpl(resp.items).appendTo('#questions');
-    
+
 }
 
-function getQuestionObject(jQuestion){
+function getQuestionObject(jQuestion) {
 
     qSummary = document.createElement('div');
     qSummary.setAttribute('class', 'question-summary');
-    
+
     qSummary.append(jQuestion.title);
     return qSummary;
 }
 
 
 function loadWarning(text) {
-    
+
     $("#warning").html("<b>WARNING: <\/b>" + text);
 
     $("#warning").slideDown("slow");
